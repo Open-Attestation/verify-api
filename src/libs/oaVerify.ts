8dbc3d8fb2e50792bfa3ab9e2520f0fd66b8b957
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-11-22 17:24:14 +0800

    fix: implement custom cache for resolver

--- src/libs/oaVerify.ts
+++ src/libs/oaVerify.ts
@@ -1,17 +1,30 @@
-import { isValid, verificationBuilder, openAttestationVerifiers, createResolver } from "@govtechsg/oa-verify";
+import { isValid, verificationBuilder, openAttestationVerifiers } from "@govtechsg/oa-verify";
 import { providers } from "ethers";
+import { Resolver, DIDCache, DIDResolutionResult } from "did-resolver";
+import { getResolver } from "ethr-did-resolver";
+import NodeCache from "node-cache";
 
 const NETWORK_NAME = process.env.NETWORK_NAME || "ropsten";
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
+const didResolutionCache = new NodeCache({ stdTTL: 1 * 60 * 60 }); // 1 hour
+const customCache: DIDCache = async (parsed, resolve) => {
+  if (parsed.params && parsed.params["no-cache"] === "true") return await resolve();
+
+  const cachedResult = didResolutionCache.get<DIDResolutionResult>(parsed.didUrl);
+  if (cachedResult) return cachedResult;
+
+  const doc = await resolve();
+  didResolutionCache.set(parsed.didUrl, doc);
+
+  return doc;
+};
+
 const provider = INFURA_API_KEY ? new providers.InfuraProvider(NETWORK_NAME, INFURA_API_KEY) : undefined;
-const resolver = INFURA_API_KEY
-  ? createResolver({
-      ethrResolverConfig: {
-        networks: [{ name: "mainnet", rpcUrl: `https://mainnet.infura.io/v3/${INFURA_API_KEY}` }],
-      },
-    })
+const ethrDidResolver = INFURA_API_KEY
+  ? getResolver({ name: NETWORK_NAME, rpcUrl: `https://${NETWORK_NAME}.infura.io/v3/${INFURA_API_KEY}` })
   : undefined;
+const resolver = new Resolver(ethrDidResolver, { cache: customCache });
 
 let verify;
 const getVerifier = () => {

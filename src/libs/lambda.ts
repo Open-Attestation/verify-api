5295535a493d9eac00cd9dc96beb831685a68ebc
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-11-22 11:38:14 +0800

    chore: add cors

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,6 +1,7 @@
 import middy from "@middy/core";
+import cors from "@middy/http-cors";
 import middyJsonBodyParser from "@middy/http-json-body-parser";
 
 export const middyfy = (handler) => {
-  return middy(handler).use(middyJsonBodyParser());
+  return middy(handler).use(middyJsonBodyParser()).use(cors());
 };

7d4fb827975a5de9d823a290d06534a999259e10
David <david_tjokroaminoto@tech.gov.sg>
2021-11-23 15:54:56 +0800

    added some basic serverless stuff

--- serverless.ts
+++ serverless.ts
@@ -1,39 +1,90 @@
 import type { AWS } from '@serverless/typescript';
+import { getArgumentValuesOrDefault, getAWSAccountId, getValueOrDefault } from "./src/libs/utils";
 
 import hello from '@functions/hello';
 
-const serverlessConfiguration: AWS = {
-  service: 'api.verify.gov.sg',
-  frameworkVersion: '2',
-  plugins: ['serverless-esbuild'],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
-    },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
-    },
-    lambdaHashingVersion: '20201221',
-  },
-  // import the function via paths
-  functions: { hello },
-  package: { individually: true },
-  custom: {
-    esbuild: {
-      bundle: true,
-      minify: false,
-      sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
-      concurrency: 10,
-    },
-  },
-};
+const STAGE = getArgumentValuesOrDefault({ flag: "stage", defaultValue: "dev" });
+
+const serverlessConfiguration = async (): Promise<AWS> => {
+    const service = 'api.verify.gov.sg';
+    const apiKeys: string[] = [`api-spm-${STAGE}-spmKey`];
+    const region  = 'ap-southeast-1';
+    const ACCOUNT_ID = await getAWSAccountId();
+    
+    return {
+        service,
+        frameworkVersion: '2',
+        plugins: [
+            'serverless-esbuild'
+        ],
+        provider: {
+            name: 'aws',
+            runtime: 'nodejs14.x',
+            stackName: "${STAGE}",
+            apiGateway: {
+                minimumCompressionSize: 1024,
+                shouldStartNameWithService: true,
+                apiKeys
+            },
+            environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+            NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+            },
+            lambdaHashingVersion: '20201221',
+        },
+        // import the function via paths
+        functions: { hello },
+        package: { individually: true },
+        custom: {
+            esbuild: {
+            bundle: true,
+            minify: false,
+            sourcemap: true,
+            exclude: ['aws-sdk'],
+            target: 'node14',
+            define: { 'require.resolve': undefined },
+            platform: 'node',
+            concurrency: 10,
+            },
+        },
+    };
+}
+
+
+
+// const serverlessConfiguration: AWS = {
+//   service: 'api.verify.gov.sg',
+//   frameworkVersion: '2',
+//   plugins: ['serverless-esbuild'],
+//   provider: {
+//     name: 'aws',
+//     runtime: 'nodejs14.x',
+//     stackName: this.service
+//     apiGateway: {
+//       minimumCompressionSize: 1024,
+//       shouldStartNameWithService: true,
+//     },
+//     environment: {
+//       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+//       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+//     },
+//     lambdaHashingVersion: '20201221',
+//   },
+//   // import the function via paths
+//   functions: { hello },
+//   package: { individually: true },
+//   custom: {
+//     esbuild: {
+//       bundle: true,
+//       minify: false,
+//       sourcemap: true,
+//       exclude: ['aws-sdk'],
+//       target: 'node14',
+//       define: { 'require.resolve': undefined },
+//       platform: 'node',
+//       concurrency: 10,
+//     },
+//   },
+// };
 
 module.exports = serverlessConfiguration;
---
+++ src/libs/utils.ts
@@ -0,0 +1,38 @@
+import { STS } from "aws-sdk";
+import createError, { HttpError } from "http-errors";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+// http-errors returns a string instead of a JSON object by default, if you simpy pass a string
+export const createHttpError = (statusCode: number, obj: Record<string, any>): HttpError => {
+  const recordString: string = JSON.stringify(obj);
+  return new createError[statusCode](recordString);
+};
+
+export const getValueOrDefault = (value: string, defaultValue: string): string => value || defaultValue;
+
+const { argv } = yargs(hideBin(process.argv));
+
+interface ArgvParams {
+  flag: string;
+  defaultValue?: string;
+}
+
+export const getArgumentValuesOrDefault = ({ flag, defaultValue = "" }: ArgvParams): string => {
+  const value = argv[flag] as string;
+  return value || defaultValue;
+};
+
+export const getAWSAccountId = async (): Promise<string> => {
+  if (process.env.IS_OFFLINE) return "123456789012";
+  else {
+    try {
+      const sts = new STS();
+      const result = await sts.getCallerIdentity().promise();
+      if (typeof result.Account !== "string") throw new Error("No account!");
+      return result.Account;
+    } catch (e) {
+      return "*";
+    }
+  }
+};

e37b334f36529ebaacae58661ed917fdf850ceee
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-04-01 14:20:57 +0800

    chore: update jest config to allow paths

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "@jest/types";
+import { pathsToModuleNameMapper } from "ts-jest";
+
+import { compilerOptions } from "./tsconfig.json";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
+};
+
+export default config;
